bef63bb68780cd47aba971020fa3584a876b3d0d
Nigel Andrews <nigel.andrews@epita.fr>
2024-01-10 17:22:48 +0100

    feat: program: setuniform macro for vec types

--- src/wrapper/program.rs
+++ src/wrapper/program.rs
@@ -21,15 +21,15 @@ impl PartialOrd for UniformLocationInfo {
 }
 
 macro_rules! set_uniform {
-    ($typename: ty => $expr: expr) => {
-        paste! {
-            pub fn [<set_uniform_ $typename>](&self, name_hash: u32, value: $typename) {
+    ($($typename: path as $fnc_name: ident => $expr: expr),+$(,)?) => {
+        $(paste! {
+            pub fn [<set_uniform_ $fnc_name>](&self, name_hash: u32, value: $typename) {
                 let loc = self.find_location(name_hash);
                 if let Some(loc) = loc {
                     $expr(self, value, loc as i32);
                 }
             }
-        }
+        })+
     };
 }
 
@@ -57,9 +57,36 @@ impl Program {
     }
 
     set_uniform! {
-        u32 => |program: &Program, value, loc| unsafe {
+        u32 as u32 => |program: &Program, value, loc| unsafe {
             gl::ProgramUniform1ui(program.handle.get(), loc, value);
-        }
+        },
+
+        f32 as f32 => |program: &Program, value, loc| unsafe {
+            gl::ProgramUniform1f(program.handle.get(), loc, value);
+        },
+
+        glm::Vec2 as vec2 => |program: &Program, value: glm::Vec2, loc| unsafe {
+            gl::ProgramUniform2f(program.handle.get(), loc, value.x, value.y);
+        },
+
+        glm::Vec3 as vec3 => |program: &Program, value: glm::Vec3, loc| unsafe {
+            gl::ProgramUniform3f(program.handle.get(), loc, value.x, value.y, value.z);
+        },
+
+        glm::Vec4 as vec4 => |program: &Program, value: glm::Vec4, loc| unsafe {
+            gl::ProgramUniform4f(program.handle.get(), loc, value.x, value.y, value.z, value.w);
+        },
+
+        // FIXME: does not work lol
+        // glm::Mat2 as mat2 => |program: &Program, value: glm::Mat2, loc| unsafe {
+        //     gl::ProgramUniformMatrix2fv(program.handle.get(), loc, 1, false, &value);
+        // },
+
+        // glm::Mat3 as mat3 => |program: &Program, value: glm::Vec3, loc| unsafe {
+        // },
+
+        // glm::Mat4 as mat4 =>|program: &Program, value: glm::Vec3, loc| unsafe {
+        // },
     }
 
     fn find_location(&self, hash: u32) -> Option<u32> {
